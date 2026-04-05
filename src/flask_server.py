29d77249e2da7af4678da3fa61928e7e5581edc2
g1ya777 <g1ya777@protonmail.com>
2026-04-05 19:48:23 +0100

    flask_server update

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 import tempfile
 from inference_file import inference
@@ -12,7 +12,10 @@ port = int(os.environ.get("PORT", 5000))
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
+
     if request.method == "POST":
+        if "audio_file" not in request.files:
+            return jsonify({"error": "No file provided"}), 400
         f = request.files["audio_file"]
         file_dir = tempfile.gettempdir() + "/file.m4a"
         f.save(file_dir)
